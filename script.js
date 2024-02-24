f71da33accbe09de09e6954a5873870aefaee598
Administrator <amntwri0323@gmail.com>
2024-02-24 18:34:32 +0530

    new way

--- script.js
+++ script.js
@@ -1,17 +1,75 @@
+
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+
+
+
+console.log("Welcome to Programiz!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Function to update the src attribute with a new random Unsplash image
+    // function updateRandomImage() {
+    //     // Create a unique timestamp to ensure a new image is fetched
+    //     // const timestamp = new Date().getTime();
+    //     var t = Math.floor(Math.random() * 10) + 1;
+        
+    //     // Get the image element by its id
+    //     var imageElement = document.getElementById('randomImage');
+
+    //     // Update the src attribute with a new random Unsplash image URL
+    //     var app= imageElement.src = `https://source.unsplash.com/random?=${t}`;
+    //     document.querySelector(".desi").innerHTML=app;
+
+    // }
+    
+    // // Call the function to set the initial image and update it when needed
+    // updateRandomImage();
+
+
+    // {name:"John",img:"https://images.unsplash.com/photo-1533173383261-e7c2432af4ec?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjN8fGRlcHJlc3NlZHxlbnwwfHwwfHx8MA%3D%3D"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1602300592242-ef0280b9bf16?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGRlcHJlc3NlZHxlbnwwfHwwfHx8MA%3D%3D"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1500099817043-86d46000d58f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FkfGVufDB8fDB8fHww"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1634759614915-4e427a5edd2b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzF8fGRlcHJlc3NlZHxlbnwwfHwwfHx8MA%3D%3D"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1541514116954-952c154301b1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHNvcnJ5fGVufDB8fDB8fHww"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1584754166504-b8f21be4e0f9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHNvcnJ5fGVufDB8fDB8fHww"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1581333100576-b73befd79a9b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fGRlcHJlc3NlZHxlbnwwfHwwfHx8MA%3D%3D"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1487297977649-04b1dc408d93?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGFsb25lfGVufDB8fDB8fHww"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1505782388-8632b3423dbb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGFsb25lfGVufDB8fDB8fHww"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1488028005574-a81ac1663de0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YWxvbmV8ZW58MHx8MHx8fDA%3D"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1604963641671-e296af98aebb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bG9ubGluZXNzfGVufDB8fDB8fHww"},
+    // {name:"John",img:"https://images.unsplash.com/photo-1515472071456-47b72fb3caff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2FkfGVufDB8fDB8fHww"},
 arr=[
-    {name:"John",img:"https://images.unsplash.com/photo-1581333100576-b73befd79a9b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fGRlcHJlc3NlZHxlbnwwfHwwfHx8MA%3D%3D"},
-    {name:"John",img:"https://images.unsplash.com/photo-1487297977649-04b1dc408d93?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGFsb25lfGVufDB8fDB8fHww"},
-    {name:"John",img:"https://images.unsplash.com/photo-1505782388-8632b3423dbb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGFsb25lfGVufDB8fDB8fHww"},
-    {name:"John",img:"https://images.unsplash.com/photo-1488028005574-a81ac1663de0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YWxvbmV8ZW58MHx8MHx8fDA%3D"},
-    {name:"John",img:"https://images.unsplash.com/photo-1604963641671-e296af98aebb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bG9ubGluZXNzfGVufDB8fDB8fHww"},
-    {name:"John",img:"https://images.unsplash.com/photo-1515472071456-47b72fb3caff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2FkfGVufDB8fDB8fHww"},
     // {name:"John",img:"https://images.unsplash.com/photo-1620389702593-ca133a35cae9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTE5fHxkZXByZXNzZWR8ZW58MHx8MHx8fDA%3D"},
-    {name:"John",img:"https://images.unsplash.com/photo-1533173383261-e7c2432af4ec?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjN8fGRlcHJlc3NlZHxlbnwwfHwwfHx8MA%3D%3D"},
-    {name:"John",img:"https://images.unsplash.com/photo-1602300592242-ef0280b9bf16?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGRlcHJlc3NlZHxlbnwwfHwwfHx8MA%3D%3D"},
-    {name:"John",img:"https://images.unsplash.com/photo-1500099817043-86d46000d58f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FkfGVufDB8fDB8fHww"},
-    {name:"John",img:"https://images.unsplash.com/photo-1634759614915-4e427a5edd2b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzF8fGRlcHJlc3NlZHxlbnwwfHwwfHx8MA%3D%3D"},
-    {name:"John",img:"https://images.unsplash.com/photo-1541514116954-952c154301b1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHNvcnJ5fGVufDB8fDB8fHww"},
-    {name:"John",img:"https://images.unsplash.com/photo-1584754166504-b8f21be4e0f9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHNvcnJ5fGVufDB8fDB8fHww"},
+    {name:"John",img:"https://source.unsplash.com/random?"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=2"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=3"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=4"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=5"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=6"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=7"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=8"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=9"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=10"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=11"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=12"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=13"},
+    // {name:"John",img:"https://source.unsplash.com/random?v=14"},
     // {name:"John",img:"https://images.unsplash.com/photo-1628922401672-af981f04ef40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGRlcHJlc3NlZHxlbnwwfHwwfHx8MA%3D%3D"},
     // {name:"John2",img:"https://images.unsplash.com/photo-1700895718129-2b5651e9f8e5?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
     // {name:"John",img:"https://images.unsplash.com/photo-1705261204230-bda3a0b1ca6f?q=80&w=1031&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
@@ -29,8 +87,21 @@ function show(){
         <img class="pic" src="${obj.img}" alt="img" />
         <div>lorem uyqw ${obj.name}</div>
       </div>`;
-        console.log(clutter)
+        // console.log(clutter)
     });
     document.querySelector(".main").innerHTML=clutter;
 }
+    
+                // var myList =arr;
+        // Function to append numbers to the list
+        function appendNumbersToList(start, end) {
+            for (var i = start; i <= end; i++) {
+                var non=`https://source.unsplash.com/random?v=${i}`
+                
+                var myDictionary = { name: 'value1',img: non };
+                arr.push(myDictionary);
+            }
+    console.log(arr);
+        }
+        appendNumbersToList(50,100);
 show();
